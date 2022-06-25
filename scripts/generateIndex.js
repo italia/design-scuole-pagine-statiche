864ca3ebc4716aa232a83ae886c7fadf9d67ce17
Andrea Stagi <stagi.andrea@gmail.com>
2022-06-25 17:28:51 +0200

    feat: improve index layout

--- scripts/generateIndex.js
+++ scripts/generateIndex.js
@@ -3,5 +3,16 @@ const Mustache = require('mustache');
 const fs = require('fs');
 
 const filesToIndex = searchInDir('build', '.html', false);
-const renderedIndex = Mustache.render(fs.readFileSync('./templates/index_tpl.html', 'utf8'), { links: filesToIndex });
+const renderedIndex = Mustache.render(
+    fs.readFileSync('./templates/index_tpl.html', 'utf8'), 
+    { 
+        links: filesToIndex,
+        removePrefix: function () {
+            return function (text, render) {
+                const page = render(text).replace('scuole-', '').replace('.html', '').replace(/\-/g, ' ')
+                return page.charAt(0).toUpperCase() + page.slice(1)
+            }
+        }
+    }
+);
 fs.writeFileSync('./build/index.html', renderedIndex)
