c91a7c795ac32ae01dcd149cd9291587b92146f5
Andrea Stagi <stagi.andrea@gmail.com>
2022-11-21 12:02:33 +0100

    chore: add minification process

--- scripts/generateIndex.js
+++ scripts/generateIndex.js
@@ -3,7 +3,7 @@ const Mustache = require('mustache');
 const fs = require('fs');
 const package = require('../package.json')
 
-const filesToIndex = searchInDir('build', '.html', false);
+const filesToIndex = searchInDir('src', '.html', false);
 const renderedIndex = Mustache.render(
     fs.readFileSync('./templates/index_tpl.html', 'utf8'), 
     { 
