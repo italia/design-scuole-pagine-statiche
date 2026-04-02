01322f167b3738c1455a58f1dc4c9e9c48b9f014
Benedettainnocenti <innocentibenedetta@gmail.com>
2026-04-02 11:28:26 +0200

    feat: implemented typescript for render cards

--- src/templates/engines/cards.ts
+++ src/templates/engines/cards.ts
@@ -6,26 +6,31 @@ import cardEditorialeHTML from '@/templates/cards/card-editoriale.html?raw';
 
 type CardType = 'editorialeStandard' | 'inlineMini' | 'informativa';
 
+interface CardData {
+  type: CardType;
+  [key: string]: string | CardType;
+} 
+
 const templates = {
   editorialeStandard: fromHTML(cardEditorialeHTML),
   inlineMini: fromHTML(cardInlineMiniHTML),
   informativa: fromHTML(cardInformativaHTML),
 };
 
-export function renderCards(listaDati: any): DocumentFragment {
+export function renderCards(listaDati: CardData[]): DocumentFragment {
   const contenitore = document.createDocumentFragment();
   /*cicla gli elementi dell'array*/
   for (const dato of listaDati) {
     //prende il template giusto in base a dato.type
     const template = templates[dato.type];
     /*genera la card*/
-    const nuovaCard = template.content.firstElementChild.cloneNode(true);
+    const nuovaCard = template.content.firstElementChild.cloneNode(true) as HTMLElement;
     /*per ogni chiave dell'array delle proprietà*/
     for (const key of Object.keys(dato)) {
       /*cerca il data-tpl*/
       const el = nuovaCard.querySelector(`[data-tpl="${key}"]`);
       /*se l'elemento esiste imposta il testo con il valore del json*/
-      if (el) el.textContent = dato[key];
+      if (el) el.textContent = dato[key] as string;
     }
 
     contenitore.appendChild(nuovaCard);
