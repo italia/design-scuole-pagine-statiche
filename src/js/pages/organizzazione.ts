e3476c754747b99362eef89eb904382f33c6f7bb
Benedettainnocenti <innocentibenedetta@gmail.com>
2026-05-07 08:52:47 +0200

    fix: removed header

--- src/js/pages/organizzazione.ts
+++ src/js/pages/organizzazione.ts
@@ -3,8 +3,6 @@ import { render, fromHTML } from '@/js/utils/templates';
 import { renderCards } from '../engines/cards';
 
 import serviceSectionHTML from '@/templates/inEvidenceSection.html?raw';
-import preheaderHTML from '@/templates/preheader.html?raw';
-import headerHTML from '@/templates/header.html?raw';
 import heroHTML from '@/templates/hero-secondario.html?raw';
 import strumentiHTML from '@/templates/tools.html?raw';
 import finanziamentiHTML from '@/templates/finanziamenti.html?raw';
@@ -16,23 +14,13 @@ import data from '@/js/pages/organizzazione.json';
 
 const templates = {
   serviceSection: fromHTML(serviceSectionHTML),
-  header: fromHTML(headerHTML),
   hero: fromHTML(heroHTML),
-  preheader: fromHTML(preheaderHTML),
   strumenti: fromHTML(strumentiHTML),
   finanziamenti: fromHTML(finanziamentiHTML),
   footer: fromHTML(footerHTML),
   rating: fromHTML(ratingHTML),
   luogo: fromHTML(cardLuogoHTML),
 };
-
-/* render header */
-const headerFragment = render(templates.header, data.mainHeader.scuola);
-
-const mainHeaderContainer = document.getElementById('main-header');
-if (mainHeaderContainer) {
-  mainHeaderContainer.appendChild(headerFragment);
-}
 
 /* render hero */
 const heroFragment = render(templates.hero, data.hero.info);
