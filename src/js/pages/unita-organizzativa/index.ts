6f24867e74dbebd45dc2c7ff0140452ccfe98757
Giulia Ghisini <giulia.ghisini@redturtle.it>
2026-05-21 16:06:04 +0200

    feat: uo view2

--- src/js/pages/unita-organizzativa/index.ts
+++ src/js/pages/unita-organizzativa/index.ts
@@ -2,20 +2,25 @@ import '@/js/main';
 // import '@/styles/sede-scolastica.css';
 import { render, fromHTML, mount } from '@/js/utils/templates';
 
+import { renderCards } from '@/js/engines/cards';
 import headerHTML from '@/templates/layout/header.html?raw';
 import footerHTML from '@/templates/layout/footer.html?raw';
 import ratingHTML from '@/templates/layout/rating.html?raw';
 import breadcrumbsHTML from '@/templates/layout/breadcrumb.html?raw';
 
-import headerPaginaFogliaHTML from '@/templates/components/pagina-foglia/header.html?raw';
+import sectionList from '@/templates/components/section-list.html?raw';
+
+import contentHeaderHTML from '@/templates/components/pagina-foglia/content-header.html?raw';
+import contentBodyHTML from '@/templates/components/pagina-foglia/content-body.html?raw';
 
 import data from './data.json';
 
 const templates = {
   header: fromHTML(headerHTML),
   breadcrumbs: fromHTML(breadcrumbsHTML),
-  headerPaginaFoglia: fromHTML(headerPaginaFogliaHTML),
-
+  contentHeader: fromHTML(contentHeaderHTML),
+  contentBody: fromHTML(contentBodyHTML),
+  sectionList: fromHTML(sectionList),
   rating: fromHTML(ratingHTML),
   footer: fromHTML(footerHTML),
 };
@@ -24,7 +29,27 @@ const templates = {
 mount('main-header', render(templates.header));
 mount('breadcrumbs', render(templates.breadcrumbs, data.breadcrumbs));
 
-mount('header-pagina', render(templates.headerPaginaFoglia, data.header));
+mount('content-header', render(templates.contentHeader, data.contentHeader));
+
+const contentBody = render(templates.contentBody, data.contentBody);
+
+contentBody
+  .querySelector('#cosa-fa')
+  ?.replaceWith(render(templates.sectionList, data.contentBody.cosaFa));
+
+const doveSiTrova = render(templates.sectionList, data.contentBody.doveSiTrova);
+doveSiTrova
+  .querySelector('[data-cards]')
+  ?.appendChild(renderCards(data.contentBody.doveSiTrova.items));
+contentBody.querySelector('#dove-si-trova')?.replaceWith(doveSiTrova);
+
+// contentBody.querySelector('#orari-di-apertura')?.appendChild(document.createTextNode(data.orariDiApertura));
+// contentBody.querySelector('#contatti')?.appendChild(document.createTextNode(data.contatti));
+// contentBody.querySelector('#dipende-da')?.appendChild(document.createTextNode(data.dipendeDa));
+// contentBody.querySelector('#responsabile')?.appendChild(document.createTextNode(data.responsabile));
+// contentBody.querySelector('#personale')?.appendChild(document.createTextNode(data.personale));
+// contentBody.querySelector('#ulteriori-informazioni')?.appendChild(document.createTextNode(data.ulterioriInformazioni));
+mount('content-body', contentBody);
 
 // // /* article */
 // mount('la-nostra-scuola', render(templates.article, data.laNostraScuola));
